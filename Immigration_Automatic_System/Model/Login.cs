83886db8011f9e2e89676502e1ddfe235f512308
Md Toriqul Islam <mdtoriqulislam08@gmail.com>
2019-04-03 22:28:44 +0600

    First_Commit

---
+++ Immigration_Automatic_System/Model/Login.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Immigration_Automatic_System.Model
+{
+    public class Login
+    {
+    }
+}
